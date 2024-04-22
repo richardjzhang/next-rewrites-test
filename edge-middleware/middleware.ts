06d75a7ed97f2c34fa110ab05ba8f7198f2cb8dd
richardjzhang <hello@richardjzhang.com>
2024-04-22 10:12:55 +1000

    Fix local run error

--- edge-middleware/middleware.ts
+++ edge-middleware/middleware.ts
@@ -6,7 +6,7 @@ export const config = {
 
 export default function middleware({ nextUrl, headers }: NextRequest) {
   const url = new URL(nextUrl);
-  url.hostname = "localhost";
+  url.hostname = "127.0.0.1";
   url.port = "4000";
 
   // localhost:3000. Seems this header is ignored.
